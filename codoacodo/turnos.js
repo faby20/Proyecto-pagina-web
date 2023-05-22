cd24f79eba62f148605f262922f41726d194c1da
faby20 <132239131+faby20@users.noreply.github.com>
2023-05-22 17:06:05 -0300

    turnos.js

--- codoacodo/turnos.js
+++ codoacodo/turnos.js
@@ -1,3 +1,16 @@
+//api mapa
+function iniciarMap(){
+    var coord = {lat:-37.90419941774776 ,lng: -57.34311016459688};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+
 // crear el objeto
 const credenciales = {
     email: "mocho@gmail.com",
